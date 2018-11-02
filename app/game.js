ec0ce179ee6cc10b2759f2dd4c101bedf3974f78
Joshua Thurston <joshthurston92@gmail.com>
2018-11-02 17:47:38 -0600

    end of class friday

--- app/game.js
+++ app/game.js
@@ -1,19 +1,33 @@
 let target = {
-  health: 10000,
-
+  health: 100,
 }
 
-document.getElementById("progress2").innerHTML = `<div id="targethealth" class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75"
+document.getElementById("healthbar2").innerHTML = `<div id="targethealth" class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75"
   aria-valuemin="0" aria-valuemax="100" style="width: ${target.health}%;"></div>`
 
 let playerHealth = 100
-document.getElementById("progress1").innerHTML = `<div class="progress-bar  bg-dark progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75"
+document.getElementById("healthbar1").innerHTML = `<div class="progress-bar  bg-dark progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75"
   aria-valuemin="0" aria-valuemax="100" style="width: 60%"></div>`;
 
-function refresh(punch, energy, ult) {
-  let template = ''
+function slap(punch, energy, ult) {
+  //check if super sayan is activated
+  // if (!sayan) {
 
+  if (punch) {
+    console.log(target.health -= punch)
+  } else if (energy) {
+    console.log(target.health -= energy)
+  } else if (ult) {
+    console.log(target.health -= ult)
+  }
+  // }
 
-  return
+  update()
+
 }
 
+function update() {
+  //update all data to the screen
+  document.getElementById("healthbar2").innerHTML = `<div id="targethealth" class="progress-bar bg-danger progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75"
+  aria-valuemin="0" aria-valuemax="100" style="width: ${target.health}%;"></div>`
+}
